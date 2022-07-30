cbd3ab125d7cfe89427e243cd47c33113e20c106
krisbobo <kiyongbohbo@gmail.com>
2022-07-30 22:26:53 +0100

    Add functions to implement required functionalities

--- src/addRemove.js
+++ src/addRemove.js
@@ -1,17 +1,21 @@
+import listClear from './listclearing';
+
 const task = document.querySelector('.input-container');
 const handleTask = document.querySelector('.todo-input');
 const userInterface = document.querySelector('list');
+const clear = document.querySelector('.btn-clear');
 
 let todos = JSON.parse(localStorage.getItem('todo-list'));
 const showToDo = () => {
   let li = '';
   if (todos) {
-    todos.forEach((todo, id) => {
+    todos.forEach((todo) => {
+      const flagged = todo.completed ? 'checked' : '';
       li += `<li class="items">
       <div class="check">
-        <label for="${id}">
-          <input type="checkbox" id="${id}" />
-          <p>${todo.description}</p>
+        <label for="">
+          <input type="checkbox" ${flagged} class="check" data-check="${todo.index}">
+          <input type="text" class="todo-input style" data-desc="${todo.index}" value="${todo.description}">
         </label>
       </div>
       <div class="icon">
@@ -62,3 +66,34 @@ userInterface.addEventListener('click', (e) => {
     taskRemoval(index);
   }
 });
+
+const update = (e) => {
+  const clicked = e.target.closest('.todo-input');
+  if (!clicked) return;
+  clicked.addEventListener('keyup', () => {
+    const task = JSON.parse(localStorage.getItem('todo-list')) || [];
+    const targetData = parseInt(clicked.getAttribute('data-desc'), 10);
+    const update = task.find((todo) => todo.index === targetData);
+    update.description = clicked.value.trim();
+    localStorage.setItem('todo-list', JSON.stringify(task));
+  });
+};
+
+userInterface.addEventListener('click', update);
+
+clear.addEventListener('click', () => {
+  const setting = JSON.parse(localStorage.getItem('todo-list')) || [];
+  const notCompleted = setting.filter((todo) => !todo.completed);
+  todos.length = 0;
+  let i = 0;
+  notCompleted.forEach((element) => {
+    element.index = i;
+    i += 1;
+  });
+
+  todos.push(...notCompleted);
+  localStorage.setItem('todo-list', JSON.stringify(todos));
+  showToDo();
+});
+
+userInterface.addEventListener('click', listClear);
